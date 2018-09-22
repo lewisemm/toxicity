85d7555adb0a5c8fe8293f90e7a9dbd175e7a0e0
Lewis Kabui <lewisemm@users.noreply.github.com>
2018-09-22 19:47:43 +0300

    Add subtract, multiply and divide functionality and tests

--- calc.py
+++ calc.py
@@ -1,2 +1,16 @@
 def add(x, y):
     return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    # py2 needs to convert either the numerator or denominator float
+    # in order to produce the correct result from divisions
+    y *= 1.0
+    if y == 0:
+        return "Sorry, cannot divide by zero!"
+    return x / y
--- test.py
+++ test.py
@@ -5,5 +5,17 @@ import calc
 class TestCalc(unittest.TestCase):
 
     def test_addition(self):
-        result1 = calc.add(2, 3)
-        self.assertEqual(5, result1)
+        result = calc.add(2, 3)
+        self.assertEqual(5, result)
+
+    def test_subtraction(self):
+        result = calc.subtract(2, 3)
+        self.assertEqual(-1, result)
+
+    def test_multiplication(self):
+        result = calc.multiply(2, 3)
+        self.assertEqual(6, result)
+
+    def test_division(self):
+        result = calc.divide(2, 3)
+        self.assertEqual(0.667, round(result, 3))
